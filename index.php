833e84df374ea6892b95f08b5161d6ff9c61eb0f
Unseile <lea.haberli@heig-vd.ch>
2025-10-31 16:42:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
 define('BASE_URL', getenv('/archidep-php-todolist-ex/') ?: '/');
 
 // Database connection parameters.
-define('DB_USER', 'todolist');
+define('DB_USER', getenv('todolist') ?: '/');
 define('DB_PASS', 'UniformiteMegalomanePreferentielUderzo');
-define('DB_NAME', 'todolist');
-define('DB_HOST', '127.0.0.1');
-define('DB_PORT', '3306');
+define('DB_NAME', getenv('todolist') ?: '/');
+define('DB_HOST', getenv('127.0.0.1') ?: '/');
+define('DB_PORT', getenv('3306') ?: '/');
 
 $db = new PDO('mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_NAME, DB_USER, DB_PASS);
 $items = array();
